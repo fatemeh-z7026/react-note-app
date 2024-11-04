c9a951ac34cfabb0fa1a33d224fe3626ab773dff
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-11-04 20:12:18 +0330

    initial commit

---
+++ note-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
