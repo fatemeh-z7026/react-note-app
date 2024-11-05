00574b33b92965d3a3ffeb7288d15973d322b7ad
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-11-05 15:12:58 +0330

    generate components

--- note-app/src/components/NoteApp/NoteApp.js
+++ note-app/src/components/NoteApp/NoteApp.js
@@ -3,11 +3,15 @@ import Note from "./Note";
 import ColorBox from "./ColorBox";
 import Grid from "@mui/material/Grid2";
 import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Add from "@mui/icons-material/Add";
 
 export default function NoteApp() {
   return (
     <div>
+    
       <Box
         sx={{
           height: "100vh",
@@ -27,14 +31,23 @@ export default function NoteApp() {
           }}
           slotProps={{
             input: {
-              style: { fontSize: "1.8rem" }, 
+              style: { fontSize: "1.8rem" },
             },
-            inputLabel:{
-                style: { fontSize: "1.8rem" }, 
-            }
+            inputLabel: {
+              style: { fontSize: "1.8rem" },
+            },
           }}
         />
         <ColorBox></ColorBox>
+        <Container sx={{display:'flex', justifyContent: "center", gap:'1rem' }}>
+          <DeleteIcon
+            sx={{ fontSize: "2.5rem", boxShadow: "0px 0px 2px 0px grey" }}
+          ></DeleteIcon>
+      
+          <Add
+            sx={{ fontSize: "2.5rem", boxShadow: "0px 0px 2px 0px grey" }}
+          ></Add>
+        </Container>
 
         <Grid
           container
